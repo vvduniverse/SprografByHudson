f5a3b3c84c5bebc1dd5b4af9373fae47a0bb9776
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-20 14:13:53 +0200

    Initial Commit

---
+++ SprografByHudson/SprografByHudsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprografByHudsonApp.swift
+//  SprografByHudson
+//
+//  Created by Vahtee Boo on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SprografByHudsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
